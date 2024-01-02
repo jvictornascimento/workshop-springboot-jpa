e66fdee4cf23e7f4fc325ab1283e60b206c8d28b
jvictornascimento <victojoao789@gmail.com>
2024-01-02 18:37:04 -0300

    User entity and controller

---
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course.controllers;
+
+import com.educandoweb.course.entites.Users;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+
+public class UserController {
+
+
+}
