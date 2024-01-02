30f64123479e0e0574e48225caa3f3099e4f702f
jvictornascimento <victojoao789@gmail.com>
2024-01-02 19:06:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.educandoweb.course.controllers;
 
-import com.educandoweb.course.entites.Users;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/educandoweb/course/entites/User.java
+++ src/main/java/com/educandoweb/course/entites/User.java
@@ -1,20 +1,27 @@
 package com.educandoweb.course.entites;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Users implements Serializable {
+@Entity
+@Table(name = "tb_user")
+
+public class User implements Serializable {
     private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public Users() {
+    public User() {
     }
 
-    public Users(Long id, String name, String email, String phone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -65,7 +72,7 @@ public class Users implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Users users)) return false;
+        if (!(o instanceof User users)) return false;
         return Objects.equals(id, users.id);
     }
 
