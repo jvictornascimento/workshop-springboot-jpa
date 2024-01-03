0af346ab4a27ce9d5c87051980a40a55e155a6a0
jvictornascimento <victojoao789@gmail.com>
2024-01-03 00:25:03 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entites/Order.java
+++ src/main/java/com/educandoweb/course/entites/Order.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -23,6 +25,8 @@ public class Order implements Serializable {
     private User client;
 
     private Integer orderStatus;
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
 
     public Order() {
@@ -66,6 +70,9 @@ public class Order implements Serializable {
         if (orderStatus != null){
             this.orderStatus = orderStatus.getCode();
         }
+    }
+    public Set<OrderItem> getItems(){
+        return items;
     }
 
     @Override
---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entites.Order;
+import com.educandoweb.course.entites.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
