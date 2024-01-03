78af178ff997a8bdd8f10228ac04dea9a2b5be26
jvictornascimento <victojoao789@gmail.com>
2024-01-03 01:08:39 -0300

    User insert

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -2,13 +2,15 @@ package com.educandoweb.course.controllers;
 
 import com.educandoweb.course.entites.User;
 import com.educandoweb.course.services.UserService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,6 +28,14 @@ public class UserController {
     public ResponseEntity<User> listById(@PathVariable Long id){
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<User> insert(@RequestBody  User user){
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
